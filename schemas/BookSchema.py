dc9355e1b2563d6fbca1b8afbdb14656b589b0bd
wolfgang1one <891460493@qq.com>
2026-06-29 18:00:14 +0800

    feat: 修改BookSchema的请求和响应参数；

--- schemas/BookSchema.py
+++ schemas/BookSchema.py
@@ -1,11 +1,21 @@
+from typing import List
+
 from pydantic import BaseModel
 
 
-class BookQuery(BaseModel):      # 用于接收请求体
+class BookQuery(BaseModel):  # 用于接收请求体
+    book_id: int
+    book_id_list: List[int]
+    author: str
     price: float
+    publisher: str
+    book_name: str
+    author: str
+    price: float
+    publisher: str
 
 
-class BookResponse(BaseModel):   # 用于返回数据（可选，方便控制输出字段）
+class BookResponse(BaseModel):  # 用于返回数据（可选，方便控制输出字段）
     id: int
     book_name: str
     author: str
@@ -13,4 +23,4 @@ class BookResponse(BaseModel):   # 用于返回数据（可选，方便控制输
     publisher: str
 
     class Config:
-        from_attributes = True   # 允许从 ORM 对象转换
+        from_attributes = True  # 允许从 ORM 对象转换
