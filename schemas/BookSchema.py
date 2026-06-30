7e2bbbe86a8fc14acce78e7e412e6494826227c9
wolfgang1one <891460493@qq.com>
2026-06-30 16:06:47 +0800

    feat: ORM;Book schema优化；

--- schemas/BookSchema.py
+++ schemas/BookSchema.py
@@ -1,23 +1,20 @@
 from typing import List, Optional
 from pydantic import BaseModel, Field
 
-class BookQuery(BaseModel):  # 用于接收请求体
-    book_id: Optional[int] = None
-    book_id_list: Optional[List[int]] = None
 
+class BookBase(BaseModel):
+    book_id: Optional[int] = None
     author: Optional[str] = None
     price: Optional[float] = None
     publisher: Optional[str] = None
     book_name: Optional[str] = None
 
 
-class BookResponse(BaseModel):  # 用于返回数据（可选，方便控制输出字段）
-    book_id: int = Field(validation_alias="id")
-    book_name: str
-    author: str
-    price: float
-    publisher: str
+class BookQuery(BookBase):  # 用于接收请求体
+    book_id_list: Optional[List[int]] = None
 
+
+class BookResponse(BookBase):  # 用于返回数据（可选，方便控制输出字段）
     class Config:
         from_attributes = True
 
